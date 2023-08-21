d60908d8e432e4f63950900afc9e75f9d20ebdad
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-08-21 16:45:53 +0600

    Fixed bugs

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,11 +3,17 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AccessTokenStrategy, RefreshTokenStrategy } from './strategies';
+import { TokenService } from 'src/utils/jwt';
 
 //Declare Auth Module
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, AccessTokenStrategy, RefreshTokenStrategy],
+  providers: [
+    AuthService,
+    AccessTokenStrategy,
+    RefreshTokenStrategy,
+    TokenService,
+  ],
 })
 export class AuthModule {}
