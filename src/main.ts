86def01eeced2b68186310fdb490b0642594eb30
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-10-12 12:50:51 +0600

    test

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,18 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function start() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  // const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   const port = 888;
+
+  //Set CORS configuration
+  app.enableCors();
 
   //Set the global prefix for our server
   app.setGlobalPrefix('api');
 
   //Set the global validation pipes for our server
   app.useGlobalPipes(new ValidationPipe());
-
-  //Set CORS configuration
-  app.enableCors();
 
   await app.listen(port, () =>
     console.log(`📢 Server starting on: http://localhost:${port}/ ⚡️`),
