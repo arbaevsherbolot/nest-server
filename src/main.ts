ea2be3f73986b461a045452568f6400f0e1e02c2
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-10-12 12:52:16 +0600

    tested

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function start() {
-  // const app = await NestFactory.create(AppModule, { cors: true });
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = 888;
 
   //Set CORS configuration
