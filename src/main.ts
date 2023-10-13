96fb10116d5e85011df7f0fdd272dfc9e3d5f6a2
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-10-14 01:01:59 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function start() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = 888;
 
   //Set CORS configuration
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
-  });
+  app.enableCors();
 
   //Set the global prefix for our server
   app.setGlobalPrefix('api');
