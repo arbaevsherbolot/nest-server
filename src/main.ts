3dbfa15093b68e5b6862aa713e73f93c69f0945c
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-10-13 11:33:10 +0600

    test cors config

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ async function start() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = 888;
 
-  //Set CORS configuration
-  app.enableCors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://arbaevs.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
   });
+
+  //Set CORS configuration
+  app.enableCors();
 
   //Set the global prefix for our server
   app.setGlobalPrefix('api');
